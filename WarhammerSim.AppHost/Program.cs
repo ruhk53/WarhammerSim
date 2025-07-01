f85afa9db91a5d1c64c2d7e2a8de04f609b7e88e
Steven Dahlstrom <Steven.Dahlstrom@nbi-sems.com>
2025-07-01 12:48:56 -0500

    check

--- WarhammerSim.AppHost/Program.cs
+++ WarhammerSim.AppHost/Program.cs
@@ -4,6 +4,10 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.WarhammerSim_ApiService>("apiservice");
 
+var password = builder.AddParameter("sql-password", secret: true);
+
+var sqlServer = builder.AddAzureSqlServer("sql").AddDatabase("WarhammerSimDB");
+
 builder.AddProject<Projects.WarhammerSim_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
